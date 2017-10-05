edab464178db7c50dece8e8b6518c30d21212058
qwerty4uiop <alexpat930@gmail.com>
2017-10-05 18:51:08 +0300

    Modified public, src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Game from './Game';
-
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
